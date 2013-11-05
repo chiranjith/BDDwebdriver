471515bfad7aa7d4d927adbfd1b41589f415aef2
=Mukilk <mukil007@gmail.com>
2013-11-05 03:54:03 -0600

    First update

---
+++ src/main/java/com/mphasis/MTAF/mainMTAF/App.java
@@ -0,0 +1,13 @@
+package com.mphasis.MTAF.mainMTAF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
